2dceead40bb85024048306aeac7c32725ccb8833
kz1324 <kz1324@naver.com>
2019-06-12 14:04:43 +0900

    14:03

--- resources/static/resources/js/index.js
+++ resources/static/resources/js/index.js
@@ -1,21 +1,71 @@
-alert("자바스크립트 연결성공!!");
+//var app;  연산자가 없어서 선언     선언과 할당을 함꼐하는것 초기화
+var app = (function(){    //바깥펑션은 앱이고 안쪽펑션은 init 
+    let init = function(){
+        login_form();
+    }  
 
-var wrapper = document.querySelector('#wrapper');  //js에서 객체가됨(컴포넌트)
-wrapper.innerHTML = '<h1>SPA 시작</h1>'
-+'<div id="target">삭제할 내용</div>'
-+'<button id="btn">지워버리자</button>'
-+'<button id="btn2">추가하자</button>';
+    let login_form = function(){       
+        var wrapper = document.querySelector('#wrapper');  //js에서 객체가됨(컴포넌트)
+        wrapper.innerHTML = '<form action="/action_page.php">'
+        +'  First name:<br>'
+        +'  <input type="text" name="firstname" value="Mickey">'
+        +'  <br>'
+        +'  Last name:<br>'
+        +'  <input type="text" name="lastname" value="Mouse">'
+        +'  <br><br>'
+        +'  <input type="submit" value="로그인" id="login_btn">'
+        +'  <input type="submit" value="회원가입" id="join_btn">'
+        +'</form> ';  
+        
+        var login_btn = document.querySelector('#login_btn');
+        var join_btn = document.querySelector('#join_btn');
 
-var btn = document.querySelector('#btn');
-var target = document.querySelector('#target');
+        join_btn.addEventListener('click', function(){           
+            wrapper.innerHTML = '<form action="/action_page.php">'
+            +' First name:<br>'
+            +' <input type="text" name="firstname" value="jeong">'
+            +' <br>'
+            +' Last name:<br>'
+            +' <input type="text" name="lastname" value="moonho">'
+            +' <br>'
+            +' age:<br>'
+            +' <input type="text" name="firstname" value="27">'
+            +' <br>'
+            +' phone:<br>'
+            +' <input type="text" name="firstname" value="01055754786">'
+            +' <br>'
+            +' address:<br>'
+            +' <input type="text" name="firstname" value="은평구">'
+            +' <br>'
+            +' <br><br>'
+            +' <input type="submit" value="로그인" id="login_btn">'
+            +' <input type="submit" value="회원가입" id="join_btn">'
+            +'</form> ';
+            });    
 
-btn.addEventListener('click', function(){
-    target.innerHTML = '';
-});
+        login_btn.addEventListener('click', function(){
+            wrapper.innerHTML = '<form action="/action_page.php">'
+            +'  First name:<br>'
+            +'  <input type="text" name="firstname" value="Mickey">'
+            +'  <br>'
+            +'  Last name:<br>'
+            +'  <input type="text" name="lastname" value="Mouse">'
+            +'  <br><br>'
+            +'  <input type="submit" value="로그인" id="login_btn">'
+            +'  <input type="submit" value="회원가입" id="join_btn">'
+            +'</form> ';  
+            });   
+    }
+    
 
-var btn2 = document.querySelector('#btn2');
+    return {init : init};   //클로저   펑션은 인풋과 아웃풋이 존재해야함 인풋은 파라미터 아웃풋은 리턴값    클로저는 함수시스템에서 맨마지막 단계 
+                 //이닛의 값을 앱에게 전달
 
-btn2.addEventListener('click', function(){
-    var temp = document.createTextNode('새로 추가됨');
-    target.appendChild(temp);
-});
+})();   
+
+
+
+
+
+
+
